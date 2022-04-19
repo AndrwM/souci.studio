10f49859842579c509c09c7772084ce5372655f4
Andrew McIntee <contact@andrew.mn>
2022-04-19 11:51:18 -0400

    Update menu overlay to properly handle a11y focusing

--- src/components/OverlayModal.js
+++ src/components/OverlayModal.js
@@ -1,12 +1,12 @@
-import { useMemo } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { useState, useEffect } from 'react';
 import classnames from 'classnames';
 import { Link } from "react-router-dom";
 
 const OverlayModal = () => {
   const [isActive, setIsActive] = useState(false);
   const [hasHover, setHasHover] = useState(false);
+  const linkTabIndex = useMemo(() => (isActive ? 0 : -1), [isActive])
 
   useEffect(() => {
     if (isActive) {
@@ -38,21 +38,22 @@ const OverlayModal = () => {
   return (
     <>
       <button
+        // ref={buttonRef}
+        // onClick={() => { setIsActive(v => !v); buttonRef.current.focus()}}
         onClick={() => setIsActive(v => !v)}
         onMouseEnter={() => setHasHover(true)}
         onMouseLeave={() => setHasHover(false)}
         className={buttonClassNames}
         children={buttonText}
-        tabIndex="0"
       />
       {ReactDOM.createPortal(
         <div className={modalClassNames}>
           <ul className="c-list c-list--xlarge">
-            <li className="c-list__item c-list__item--wave"><a href="aa" className="c-link">About Me <span className="u-subtler">&nbsp;(&nbsp;You are here&nbsp;)</span></a></li>
-            <li className="c-list__item c-list__item--remote"><a href="aa" className="c-link c-link--arrow-remote">LinkedIn</a></li>
-            <li className="c-list__item c-list__item--remote"><a href="aa" className="c-link c-link--arrow-remote">Github</a></li>
-            <li className="c-list__item c-list__item--diamond"><a href="aa" className="c-link">Colophon</a></li>
-            <li className="c-list__item c-list__item--wave"><Link to="/wayback-machine" onClick={() => setIsActive(false)} className="c-link">Wayback: 2019</Link></li>
+            <li className="c-list__item c-list__item--wave"><a href="aa" tabIndex={linkTabIndex} className="c-link">About Me <span className="u-subtler">&nbsp;(&nbsp;You are here&nbsp;)</span></a></li>
+            <li className="c-list__item c-list__item--remote"><a href="aa" tabIndex={linkTabIndex} className="c-link c-link--arrow-remote">LinkedIn</a></li>
+            <li className="c-list__item c-list__item--remote"><a href="aa" tabIndex={linkTabIndex} className="c-link c-link--arrow-remote">Github</a></li>
+            <li className="c-list__item c-list__item--diamond"><a href="aa" tabIndex={linkTabIndex} className="c-link">Colophon</a></li>
+            <li className="c-list__item c-list__item--wave"><Link to="/wayback-machine" tabIndex={linkTabIndex} onClick={() => setIsActive(false)} className="c-link">Wayback: 2019</Link></li>
           </ul>
         </div>,
         document.body
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,16 +1,20 @@
+import ReactDOM from 'react-dom';
 import { Outlet } from 'react-router-dom';
 import ThemeToggle from '../components/ThemeToggle';
 import OverlayModal from "../components/OverlayModal";
 
 const Layout = () => (<>
-  <div className='c-overlay-hud'>
-    <div className='c-overlay-hud__col-primary'>
-      <OverlayModal />
-    </div>
-    <div className='c-overlay-hud__col'>
-      <ThemeToggle />
-    </div>
-  </div>
+  {ReactDOM.createPortal(
+    <div className='c-overlay-hud'>
+      <div className='c-overlay-hud__col-primary'>
+        <OverlayModal />
+      </div>
+      <div className='c-overlay-hud__col'>
+        <ThemeToggle />
+      </div>
+    </div>,
+    document.body
+  )}
   <Outlet />
 </>);
 
