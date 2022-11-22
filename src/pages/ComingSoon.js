fffa908f7689fb0d85e12b52db837fe25a4751c6
Andrew McIntee <contact@andrew.mn>
2022-11-22 12:48:57 -0500

    s

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -15,9 +15,9 @@ const Intro = () => (<>
     <Text type="paragraph">
       <span className="u-highlight">I’m a software engineer and product designer</span>, pushing <span className="u-highlight">thirteen years</span> of <span className="u-highlight">building web software</span>.
     </Text>
-    <Text type="title" modifier="small" className="u-margin-top">Expertise</Text>
+    <Text type="title" modifier="small" className="u-margin-top">Experience</Text>
     <Text type="paragraph">
-      For 10 years I specialized on the client&#8209;side. I designed and built software user interfaces for many organizations: from scrappy startups, to large SAAS softwares, and even larger entities like BMW, Outdoor Channel, and Home&nbsp;Depot. I’m now <span className="u-highlight">focused server&#8209;side</span>&ndash; building, maintaining, and measuring features for a multi&#8209;billion GMV software product with Ruby, Javascript, and Go.
+      For ten years I specialized on the client&#8209;side. I designed and built software user interfaces for many organizations: from scrappy startups, to large SAAS softwares, and even larger entities like BMW, Outdoor&nbsp;Channel, and Home&nbsp;Depot. I’m now <span className="u-highlight">focused server&#8209;side</span>&ndash;building, maintaining, and measuring features for multi&#8209;billion GMV software products with Ruby, Javascript, and Go.
     </Text>
     <Text type="title" modifier="small" className="u-margin-top">For whom</Text>
     <Text type="paragraph">
@@ -38,7 +38,12 @@ const Intro = () => (<>
     />
   </div>
   <div className="c-wrapper u-padding-top-large">
-    <Text type="title" modifier="medium" className="u-margin-top">Experience</Text>
+    <Text type="title" modifier="medium" className="u-margin-top">A brief history&hellip;</Text>
+    <Text type="paragraph">
+      I identify as a software engineer and product designer switch&#8209;hitter.
+      Throughout my career, I have continued to move further down the techstack.
+      I'm most at home in cross&#8209;functional engineering teams, where each member wears multiple hats.
+    </Text>
     <HtmlReveal tag="table" className="u-margin-bottom">
       <thead>
         <tr>
@@ -89,22 +94,17 @@ const Intro = () => (<>
       <tfoot>
         <tr>
           <th scope="row"></th>
-          <td><span className="c-emoji">🔗</span></td>
+          <td><span className="c-emoji c-emoji--small">🔗</span></td>
           <td>
             <a href="https://2019.andrew.mn/#/case-studies" rel="noopener noreferrer nofollow" target="_blank" className="c-link">LinkedIn</a>
           </td>
-          <td><span className="c-emoji">🔗</span></td>
+          <td><span className="c-emoji c-emoji--small">🔗</span></td>
           <td>
             <a href="https://2019.andrew.mn/#/case-studies" rel="noopener noreferrer nofollow" target="_blank" className="c-link">Case studies (2019 archive)</a>
           </td>
         </tr>
       </tfoot>
     </HtmlReveal>
-    <Text type="paragraph">
-      I identify as a software engineer and product designer switch&#8209;hitter.
-      Throughout my career, I have continued to move further down the techstack.
-      I'm most at home in cross&#8209;functional engineering teams, where each member wears multiple hats.
-    </Text>
     <Text type="paragraph" className="u-margin-top">
       Thanks for visting. <span className="u-highlight">Have a great day</span>. <span className="c-emoji">🌿</span>
     </Text>
